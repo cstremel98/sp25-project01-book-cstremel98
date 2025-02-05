37049bab3c9ca6f0c4eb0b5132d35d3539070c1c
Charles Stremel <cstremel@csumb.edu>
2025-02-05 15:46:48 -0800

    All of Book.java except equals(), hashcode(), and dueDate in constructor.

--- Book.java
+++ Book.java
@@ -3,7 +3,7 @@
  * @author Charles Stremel
  */
 
-import java.util.LocalDate;
+import java.time.LocalDate;
 
 public class Book {
 	public static final int ISBN_ = 0;
@@ -17,17 +17,35 @@ public class Book {
 	private String title;
 	private String subject;
 	private String author;
-
-	private int page_count;
-
+	private int pageCount;
 	private LocalDate dueDate;
 
-	public Book(){
+	public Book(String isbn, String title, String subject, String author, int pageCount){
 		this.isbn = isbn;
+		this.title = title;
+		this.subject = subject;
+		this.author = author;
+		this.pageCount = pageCount;
 	}
 
 	@Override
 	public final boolean equals(Object o) {
+		if(this == o) {
+			return true;
+		}	
+		if(o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Book book = (Book) o;
+
+		if(getPageCount() != book.getPageCount()) {
+			return false;
+		}
+		//WIP
+		if(getISBN() != null ? !getISBN().equals(book.getISBN()) : book.getISBN != null ...)
+			return false;
+		}
 	}
 
 	@Override
@@ -35,9 +53,42 @@ public class Book {
 		return title + " by " + author + " ISBN: " + isbn;
 	}
 
-	public String getIsbn() {
+	public String getAuthor() {
+		return isbn;
+	}
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+	public String getISBN() {
+		return isbn;
+	}
+	public String getPageCount() {
+		return isbn;
+	}
+	public String getSubject() {
+		return isbn;
+	}
+	public String getTitle() {
 		return isbn;
 	}
 	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+	public void setISBN(String isbn) {
+		this.isbn = isbn;
+	}
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 }
--- BookTest.java
+++ BookTest.java
@@ -1,3 +1,18 @@
-/*
- *
+/**
+ * Main to test Book class.
+ * 1. Create a null Book.
+ * 2. Assert it is null.
+ * 3. Instantiate with empty parameters.
+ * 4. Assert it is not null.
+ * @author Charles Stremel
  */
+
+public class BookTest {
+	public static void main(String [] args) {
+		Book book = new Book("4523", "Dune", "Sci-fi", "Frank Herbert", 500);
+	}
+
+	public static boolean test1() {
+		return true;
+	}
+}
