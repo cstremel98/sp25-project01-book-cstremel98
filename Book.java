c666f8369027e30ec36defebf1f7b6b1f361f81f
Charles Stremel <cstremel@csumb.edu>
2025-02-08 21:16:50 -0800

    Completed files with successful tests.

--- Book.java
+++ Book.java
@@ -4,6 +4,7 @@
  */
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Book {
 	public static final int ISBN_ = 0;
@@ -17,35 +18,30 @@ public class Book {
 	private String title;
 	private String subject;
 	private String author;
-	private int pageCount;
+
+	private int page_count;
+
 	private LocalDate dueDate;
 
-	public Book(String isbn, String title, String subject, String author, int pageCount){
+	public Book(String isbn, String title, String subject, String author, int page_count, LocalDate dueDate) {
 		this.isbn = isbn;
 		this.title = title;
 		this.subject = subject;
 		this.author = author;
-		this.pageCount = pageCount;
+		this.page_count = page_count;
+		this.dueDate = dueDate;
 	}
 
 	@Override
-	public final boolean equals(Object o) {
-		if(this == o) {
-			return true;
-		}	
-		if(o == null || getClass() != o.getClass()) {
-			return false;
-		}
-
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
 		Book book = (Book) o;
+		return page_count == book.page_count && Objects.equals(isbn, book.isbn) && Objects.equals(title, book.title) && Objects.equals(subject, book.subject) && Objects.equals(author, book.author);
+	}
 
-		if(getPageCount() != book.getPageCount()) {
-			return false;
-		}
-		//WIP
-		if(getISBN() != null ? !getISBN().equals(book.getISBN()) : book.getISBN != null ...)
-			return false;
-		}
+	@Override
+	public int hashCode() {
+		return Objects.hash(isbn, title, subject, author, page_count);
 	}
 
 	@Override
@@ -53,42 +49,43 @@ public class Book {
 		return title + " by " + author + " ISBN: " + isbn;
 	}
 
-	public String getAuthor() {
+	public String getIsbn() {
 		return isbn;
 	}
-	public LocalDate getDueDate() {
-		return dueDate;
-	}
-	public String getISBN() {
-		return isbn;
-	}
-	public String getPageCount() {
-		return isbn;
-	}
-	public String getSubject() {
-		return isbn;
-	}
-	public String getTitle() {
-		return isbn;
-	}
-	
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public void setDueDate(LocalDate dueDate) {
-		this.dueDate = dueDate;
-	}
-	public void setISBN(String isbn) {
+
+	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-	public void setPageCount(int pageCount) {
-		this.pageCount = pageCount;
+
+	public String getTitle() {
+		return title;
 	}
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getPage_count() {
+		return page_count;
+	}
+
+	public void setPage_count(int page_count) {
+		this.page_count = page_count;
+	}
 }
--- BookTest.java
+++ BookTest.java
@@ -1,18 +1,110 @@
-/**
- * Main to test Book class.
- * 1. Create a null Book.
- * 2. Assert it is null.
- * 3. Instantiate with empty parameters.
- * 4. Assert it is not null.
- * @author Charles Stremel
- */
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class BookTest {
-	public static void main(String [] args) {
-		Book book = new Book("4523", "Dune", "Sci-fi", "Frank Herbert", 500);
-	}
+import static org.junit.jupiter.api.Assertions.*;
 
-	public static boolean test1() {
-		return true;
-	}
+import java.time.LocalDate;
+
+class BookTest {
+
+    Book book, book2, book3;
+    Book nullBook;
+
+    String isbn = "0123", isbn2 = "0456";
+    String title = "How to Learn Java", title2 = "How to Learn to Swim";
+    String subject = "Computer Science", subject2 = "Athletics";
+    String author = "John McAfee", author2 = "John Carmac";
+    int page_count = 500, page_count2 = 300;
+    LocalDate dueDate = LocalDate.of(2025, 2, 9);
+
+    @BeforeEach
+    void setUp() {
+        nullBook = null;
+        book = new Book(isbn, title, subject, author, page_count, dueDate);
+        book2 = new Book(isbn2, title2, subject2, author2, page_count2, dueDate);
+        book3 = new Book(isbn2, title2, subject2, author2, page_count2, dueDate);
+    }
+
+    @AfterEach
+    void tearDown() {
+        book = null;
+    }
+
+    @Test
+    void constructorTest() {
+        assertEquals(nullBook, null);
+        assertNotEquals(book, null);
+    }
+
+    @Test
+    void testEquals() {
+        assertNotEquals(book, book2);
+        assertEquals(book2, book3);
+    }
+
+    @Test
+    void getIsbn() {
+        assertEquals(isbn, book.getIsbn());
+    }
+
+    @Test
+    void setIsbn() {
+        assertEquals(isbn, book.getIsbn());
+        book.setIsbn(isbn2);
+        assertNotEquals(isbn, book.getIsbn());
+        assertEquals(book.getIsbn(), book2.getIsbn());
+    }
+
+    @Test
+    void getTitle() {
+        assertEquals(title, book.getTitle());
+    }
+
+    @Test
+    void setTitle() {
+        assertEquals(title, book.getTitle());
+        book.setTitle(title2);
+        assertNotEquals(title, book.getTitle());
+        assertEquals(book.getTitle(), book2.getTitle());
+    }
+
+    @Test
+    void getSubject() {
+        assertEquals(subject, book.getSubject());
+    }
+
+    @Test
+    void setSubject() {
+        assertEquals(subject, book.getSubject());
+        book.setSubject(subject2);
+        assertNotEquals(subject, book.getSubject());
+        assertEquals(book.getSubject(), book2.getSubject());
+    }
+
+    @Test
+    void getAuthor() {
+        assertEquals(author, book.getAuthor());
+    }
+
+    @Test
+    void setAuthor() {
+        assertEquals(author, book.getAuthor());
+        book.setAuthor(author2);
+        assertNotEquals(author, book.getAuthor());
+        assertEquals(book.getAuthor(), book2.getAuthor());
+    }
+
+    @Test
+    void getPage_count() {
+        assertEquals(page_count, book.getPage_count());
+    }
+
+    @Test
+    void setPage_count() {
+        assertEquals(page_count, book.getPage_count());
+        book.setPage_count(page_count2);
+        assertNotEquals(page_count, book.getPage_count());
+        assertEquals(book.getPage_count(), book2.getPage_count());
+    }
 }
